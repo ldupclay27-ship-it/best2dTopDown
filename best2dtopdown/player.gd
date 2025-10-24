fd8417f690f8f3e57433f019eeecb704d8539b10
ldupclay27-ship-it <ldupclay27@bfhsla.org>
2025-10-24 15:21:12 -0500

    background added

--- best2dtopdown/player.gd
+++ best2dtopdown/player.gd
@@ -7,4 +7,14 @@ func _physics_process(delta: float) -> void:
 	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = move_vector * move_speed
 	
+	if velocity.x > 0:
+		$AnimatedSprite2D.play("walk_right")
+	elif velocity.x < 0:
+		$AnimatedSprite2D.play("walk_left")
+	elif velocity.y > 0:
+		$AnimatedSprite2D.play("walk_down")
+	elif velocity.y < 0:
+		$AnimatedSprite2D.play("walk_up")
+	else:
+		$AnimatedSprite2D.stop()
 	move_and_slide()
