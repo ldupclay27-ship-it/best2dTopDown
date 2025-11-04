deaaa19cc75f918436531ffff2119d214074f68a
ldupclay27-ship-it <ldupclay27@bfhsla.org>
2025-11-04 15:23:36 -0600

    day 3

---
+++ best2dtopdown/portal.gd
@@ -0,0 +1,27 @@
+extends Area2D
+@export var next_scene : PackedScene 
+@export var player_location : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	
+	if body is Player:
+		print("entered the portal")
+		get_tree().change_scene_to_file.call_deferred(next_scene)
+	
+
+
+func _on_body_exited(body: Node2D) -> void:
+	#print("exited the portal")
+	pass
